fbf4d26ce10a5b96ef7378f116af90159abdfea4
keshgurung <k_grg@hotmail.com>
2022-05-16 23:19:59 +0100

    single event page with getstaticprops and getstaticpath function

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # EventaApp-Nextjs
-A beautiful events app with React Nextjs framework.
+
+A beautiful events app build with React Nextjs framework. 
+
+### Planning: 
+ - have four main routes.
+ - starting page shows list of featured events only not all the events. (/)
+ - events folder and shows all the events. (/events)
+ - a single dynamic page that showa all info abt the event. (/events/${id})
+ - search bar for the user (/events/...slug) 
+
+
--- myeventsapp/pages/events/[eventid].js
+++ myeventsapp/pages/events/[eventid].js
@@ -1,34 +1,54 @@
-import React from 'react'
-import {useRouter} from 'next/router'
-import {getEventById} from '../../dummy-data'
-import { Fragment } from 'react'
-import EventSummary from '../../components/event-detail/event-summary'
-import EvenLogistics from '../../components/event-detail/event-logistics'
-import EventContent from '../../components/event-detail/event-content'
+import React from "react";
+// import {useRouter} from 'next/router'
+import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EvenLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
-const singleEvent = () => {
-  const router = useRouter()
+const singleEvent = (props) => {
+  // const router = useRouter()
 
-  const eventId = router.query.eventid
-  const event = getEventById(eventId)
+  // const eventId = router.query.eventid
+  const event = props.selectedEvent;
 
-  if (!event){
-    return <p>no event found</p>
+  if (!event) {
+    return <p>no event found</p>;
   }
 
   return (
     <Fragment>
-      <EventSummary title={event.title}/>
-      <EvenLogistics 
-      date={event.date} 
-      address={event.location} 
-      image={event.image} 
-      alt={event.title} />
+      <EventSummary title={event.title} />
+      <EvenLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        alt={event.title}
+      />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
     </Fragment>
-  )
+  );
+};
+export async function getStaticProps(context) {
+  const eventId = context.params.eventid;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
 }
 
-export default singleEvent
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({ params: { eventid: event.id } }));
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+export default singleEvent;
